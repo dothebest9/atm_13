9008452a02af606e8011d1d813bf24aced946ca4
dothebest9 <smrmtjsrn11@naver.com>
2025-07-18 11:34:42 +0900

    ✨ 오류 수정
    들여쓰기 오류 수정

--- atm.py
+++ atm.py
@@ -9,7 +9,7 @@ while True:
     if num == '1':#입금 기능 구현 -> feat/deposit 브랜치에서 작업
         deposit_amount=int(input("입금할 금액을 입력 해주세요:"))#str:5000 ->int ->
         if deposit_amount <= 0:
-        balance += deposit_amount #balance=10000+deposit_amount
+            balance += deposit_amount #balance=10000+deposit_amount
         print(f'입금하신 금액은{deposit_amount}원이고, 현재 잔액은{balance}원입니다')
     if num == '2':
         withdraw_amount=int(input("출금할 금액을 입력 해주세요:"))
